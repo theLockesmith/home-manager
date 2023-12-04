f576296be1adef550e843ae13bdbc2d3247975ec
lockesmith <lockesmith@coldforge.xyz>
2023-12-04 11:01:56 -0500

    changed colors to work more often

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in {
-      homeConfigurations."forgemaster" = home-manager.lib.homeManagerConfiguration {
+      homeConfigurations."ccoleman" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
 
         # Specify your home configuration modules here, for example,
