4206adffa82b5b53c46a510ad4a2b59818a5cf48
Chuck Coleman <chuck.coleman@empireaccess.com>
2023-12-04 13:03:06 -0500

    added nix to home-manager config

--- home.template.nix
+++ home.template.nix
@@ -1,6 +1,11 @@
 { config, pkgs, ... }:
 
 {
+  nix = {
+    package = pkgs.nix;
+    settings.experimental-features = [ "nix-command" "flakes" ];
+  };
+
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "user_placeholder";
