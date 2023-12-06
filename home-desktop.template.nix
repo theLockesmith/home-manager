fbe27b2febd0bf052470bf4b78b78f819bcaa9fa
Chuck Coleman <chuck.coleman@empireaccess.com>
2023-12-06 10:49:43 -0500

    breaking home.nix down into common packages and specific package sets

--- home-desktop.template.nix
+++ home-desktop.template.nix
@@ -1,27 +1,26 @@
-{ config, pkgs, ... }:
-
 let
+    # Import base config
     common = import ./home.nix;
 
 in
 common // {
-    home.file = {
+    home.file = common.home.files ++ {
 
     };
 
-    home.packages = [
+    home.packages = common.home.packages ++ [
         ## Desktop packages
-        pkgs.keepassxc
-        pkgs.ffmpeg-full
-        pkgs.glxinfo
-        pkgs.pavucontrol
+        common.pkgs.keepassxc
+        common.pkgs.ffmpeg-full
+        common.pkgs.glxinfo
+        common.pkgs.pavucontrol
         # Half assed fix for nextcloud: https://github.com/NixOS/nixpkgs/issues/82959#issuecomment-761541061
-        pkgs.nextcloud-client
-        pkgs.vscodium
-        pkgs.firefox
+        # pkgs.nextcloud-client
+        common.pkgs.vscodium
+        common.pkgs.firefox
     ];
 
-    home.sessionVariables = {
+    home.sessionVariables = common.home.sessionVariables ++ {
         # EDITOR = "emacs";
     };
 }
